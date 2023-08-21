ede1e0c0cf5f2f14cb6bd55640535649f36b8291
senthilbit <111846860+senthilbit@users.noreply.github.com>
2023-08-21 17:09:07 +0530

    deleting extra runner files

--- src/test/java/Runner/UIAutomation_RunAction.java
+++
@@ -1,9 +0,0 @@
-package Runner;
-
-import com.intuit.karate.junit5.Karate;
-public class UIAutomation_RunAction {
-    @Karate.Test
-    Karate UIAutomation(){
-        return Karate.run("classpath:Features").tags("UI_Automation").relativeTo((getClass()));
-    }
-}
