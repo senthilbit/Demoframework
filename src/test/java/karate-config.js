e3d28be05c1c8477a96943f0ebcdff30455a4a9d
senthilbit <111846860+senthilbit@users.noreply.github.com>
2023-08-25 21:55:54 +0530

    Mobile Autoamation

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,29 +1,39 @@
-function fn(){
-	
-	var config = {
-		
-		name : "Akshay" ,
-		tokenID : "fa100e7b3ed6a1f7203c4e370469c0dbdfb794bfbe74e1b547ae77bb2a12a1fd" ,
-		baseUrl : "https://gorest.co.in"
-		
-	};
-	
-	var env = karate.env
-	karate.log('The value of env is : ', env);
-	
-	if(env == 'qa'){
-		
-		config.baseUrl = "https://gorest.co.in"
-		
-	} else if(env == 'dev')
-	{
-		config.baseUrl = "https://gorest.co.in"
-	}else
-	{
-		config.baseUrl = "https://gorest.co.in"
-	}
+function fn() {
+  var config = {}
+  var android = {}
 
-	
-	
-	return config;
+   android["desiredConfig"] = {
+     "app" : "C:/Users/sonalp/Downloads/UiDemo.apk",
+     "newCommandTimeout" : 300,
+     "platformVersion" : "9.0",
+     "platformName" : "Android",
+     "connectHardwareKeyboard" : true,
+     "deviceName" : "emulator-5554",     "avd" : "Galaxy_Nexus_API_34",
+     "automationName" : "UiAutomator2"
+   }
+  config = {
+
+  		name : "Akshay" ,
+  		tokenID : "fa100e7b3ed6a1f7203c4e370469c0dbdfb794bfbe74e1b547ae77bb2a12a1fd" ,
+  		baseUrl : "https://gorest.co.in"
+
+  };
+  config["android"] = android
+
+  var env = karate.env
+  	karate.log('The value of env is : ', env);
+
+  	if(env == 'qa'){
+
+  		config.baseUrl = "https://gorest.co.in"
+
+  	} else if(env == 'dev')
+  	{
+  		config.baseUrl = "https://gorest.co.in"
+  	}else
+  	{
+  		config.baseUrl = "https://gorest.co.in"
+  	}
+
+  return config;
 }
