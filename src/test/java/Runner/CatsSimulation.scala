6022e58956c0e80d9809ecba087b3cc9d2796d4a
GurjarA <GurjarA@IN-GNR-LPT299.thedigitalgroup.com>
2023-09-25 15:50:39 +0530

    Changes with Assertions and load Distribution

--- src/test/java/Runner/CatsSimulation.scala
+++ src/test/java/Runner/CatsSimulation.scala
@@ -9,7 +9,7 @@ import scala.language.postfixOps
 
 class CatsSimulation extends Simulation {
 
-  MockUtils.startServer(0)
+   MockUtils.startServer(0)
 
   val protocol = karateProtocol(
     "/cats/{id}" -> Nil,
@@ -27,12 +27,39 @@ class CatsSimulation extends Simulation {
   val delete = scenario("delete").group("delete cats") {
     exec(karateFeature("classpath:Features/cats-delete.feature@name=delete"))
   }
-  val custom = scenario("custom").exec(karateFeature("classpath:Features/custom-rpc.feature"))
+
+  val custom = scenario("SearchTest").
+    group("SearchTesting") {
+      exec(karateFeature("classpath:Features/custom-rpc.feature"))
+    }
+
+
 
   setUp(
-    create.inject(rampUsers(100) during (5 seconds)).protocols(protocol),
-    delete.inject(rampUsers(50) during (5 seconds)).protocols(protocol),
-    custom.inject(rampUsers(100) during (5 seconds)).protocols(protocol)
-  )
+    create.inject(
+      nothingFor(5 seconds), // Pause for a given duration
+      atOnceUsers(10), //Inject 10 Users at once
+      constantUsersPerSec(10) during (20 seconds), // Induce 10 requests on every second and continues this process for 30 seconds
+      rampUsers(10) during (50 seconds) // Linear Ramp up of the user
+    ).protocols(protocol),
+
+    delete.inject(
+      nothingFor(10 seconds), // Pause for a given duration
+      atOnceUsers(20), // Inject 10 Users at once
+      constantUsersPerSec(10) during (10 seconds), // Induce 10 requests on every second and continues this process for 40 seconds
+    ).protocols(protocol),
+
+    custom.inject(
+      nothingFor(15 seconds), // Pause for a given duration
+      rampUsers(20) during (50 seconds) // Linear Ramp up of the user
+    ).protocols(protocol),
+
+
+
+  ).assertions(
+    global.responseTime.max.between(100, 5000),
+    global.failedRequests.percent.between(0,90),
+    global.successfulRequests.percent.gt(80)
+  ).maxDuration(2 minutes) // Configuring the maximum duration of your simulation. It is useful when we need to bound the duration the simulation when we can’t predict it.
 
 }
